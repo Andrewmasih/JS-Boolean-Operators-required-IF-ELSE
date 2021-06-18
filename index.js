c9dd01767e6a1f672456bcd722c55ea75e5c33b8
Andrew Masih <armasih@outlook.com>
2021-06-18 00:12:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/* If the condition is TRUE, then the IF statement will be executed, otherwise, the ELSE code will be RUN */
+
+let a = 10;
+let b = 20;
+let result;
+
+if (a<b) {
+  result = "a is smaller";
+} else {
+  result = "a is not smaller";
+}
+
+console.log(result);
+
+/* Try changing the condition values and see the result */
